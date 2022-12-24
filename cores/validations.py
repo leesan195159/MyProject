f15a4517246433a9ed992d5a958804e73436be38
leesan195159 <sniping1997@naver.com>
2022-12-24 23:54:28 +0900

    users views 및 url 구현

---
+++ cores/validations.py
@@ -0,0 +1,16 @@
+import re
+
+from django.forms import ValidationError
+
+# 이메일  - 이메일 형식
+# 비밀번호 - 최소 한개의 영문자 + 최소 한개의 숫자 + 최소 8자
+EMAIL_REGEX    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$"
+
+def validate_email(email):
+    if not re.match(EMAIL_REGEX, email):
+        raise ValidationError('Invalid_Email')
+
+def validate_password(password):
+    if not re.match(PASSWORD_REGEX, password):
+        raise ValidationError('Invalid_Password')
---
+++ users/views.py
@@ -0,0 +1,68 @@
+import json
+import jwt
+import bcrypt
+
+from django.views import View
+from django.http  import JsonResponse
+from django.forms import ValidationError
+from datetime     import datetime, timedelta
+
+from django.conf       import settings
+from cores.validations import validate_email, validate_password
+from users.models      import User
+
+class SignIn(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            name     = data['user_name']
+            email    = data['email']
+            password = data['password']
+
+            validate_email(email)
+            validate_password(password)
+
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'message': 'Already_Exist_Email'}, status=402)
+
+            User.objects.create(
+                name     = name,
+                email    = email,
+                password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            )
+
+            return JsonResponse({
+                'message': 'Success'
+            })
+        except KeyError:
+            return JsonResponse({'message': 'Key_Error'}, status=401)
+        except ValidationError:
+            return JsonResponse({'message': 'Validation_Error'}, status=403)
+
+class LogIn(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+            user     = User.objects.get(email=email)
+
+            validate_email(email)
+            validate_password(password)
+
+            if User.objects.filter(email=email, password=password).exists():
+                token = jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(days=1)},
+                                settings.SECRET_KEY,
+                                settings.ALGORITHM)
+                return JsonResponse({
+                    'message': 'Success',
+                    'token': token
+                })
+            else:
+                return JsonResponse({'message': 'Login_Error'}, status=406)
+        except KeyError:
+            return JsonResponse({'message': 'Key_Error'}, status=401)
+        except ValidationError:
+            return JsonResponse({'message': 'Validation_Error'}, status=403)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'Does_Not_Exist_Error'}, status=407)
