86cd5d800da9efcd499f97baf9d440c1185d5df5
leesan195159 <sniping1997@naver.com>
2022-12-25 08:05:28 +0900

    url 추가 및 코드 컨벤션에 따라 정리

--- accountbook/urls.py
+++ accountbook/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from accountbook.views import PostAccountBook, GetAccountBook, PatchAccountBook, DeleteAccountBook
+from accountbook.views import PostAccountBook, GetAccountBook, PatchAccountBook, DeleteAccountBook, AmountDetailUrl
 
 
 urlpatterns = [
     path('/post', PostAccountBook.as_view()),
     path('/get', GetAccountBook.as_view()),
     path('/patch/<int:accountbook_id>', PatchAccountBook.as_view()),
-    path('/delete', DeleteAccountBook.as_view())
+    path('/delete', DeleteAccountBook.as_view()),
+    path('/url/<int:accountbook_id>', AmountDetailUrl.as_view())
 ]
--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from cores.timestamp import TimestampZone
 
+
 class User(TimestampZone):
     name     = models.CharField(max_length=30)
     email    = models.EmailField(max_length=128, unique=True)
